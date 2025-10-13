abad1330b8460996ffb6d8cec8a703e53175a4f1
Lamerque Araújo <lamerquea12@gmail.com>
2025-10-13 14:30:47 -0300

    Adiciona marcação fixa nas página principais, para evidência a página atual + Transfere dados da página de compra para a de orçamento, deixando assim já marcado as opções referentes ao produto na página de orçamento

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Alooo");
+//Ativar links do Menu
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(params) {
+  const element = document.getElementById(params);
+  if (element) element.checked = true
+}
+
+parametros.forEach(ativarProdutos);
