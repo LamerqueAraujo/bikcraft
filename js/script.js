1c198a2ce5f72a20942907eab5a00a0a3152a046
Lamerque Araújo <lamerquea12@gmail.com>
2025-10-13 15:39:27 -0300

    Adiciona efeitos na sessão de perguntas e uma galeria de imagens

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Ativar links do Menu
 
-const links = document.querySelectorAll(".header-menu a");
+const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
   const url = location.href;
@@ -23,3 +23,40 @@ function ativarProdutos(params) {
 }
 
 parametros.forEach(ativarProdutos);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(e) {
+  const pergunta = e.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const responta = document.getElementById(controls);
+
+  responta.classList.toggle('ativa');
+  const respostaAtiva = responta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', respostaAtiva)
+
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de bicicletas 
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+galeria.forEach((img) => {
+  img.addEventListener('click', (e) => {
+    const img = e.currentTarget;
+    const media = matchMedia('(min-width: 1000px)',).matches;
+    if (media === true) {
+      galeriaContainer.prepend(img)
+    }
+  })
+});
